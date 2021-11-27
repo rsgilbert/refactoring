9a91c5134fe312202e666d8091a2aa6e6fe48825
rsgilbert <gssenyonjo@gmail.com>
2021-11-27 17:37:16 +0000

    Initial commit. Refactoring by Martin Fowler

---
+++ ch1/data.js
@@ -0,0 +1,30 @@
+// plays
+const plays = {
+    "hamlet": {"name": "Hamlet", "type": "tragedy"},
+    "as­like": {"name": "As You Like It", "type": "comedy"},
+    "othello": {"name": "Othello", "type": "tragedy"}
+}
+    
+// invoices 
+const invoices = [
+    {
+        "customer": "BigCo",
+        "performances": [
+            {
+            "playID": "hamlet",
+            "audience": 55
+            },
+            {
+            "playID": "as­like",
+            "audience": 35
+            },
+            {
+            "playID": "othello",
+            "audience": 40
+            }
+        ]
+    }
+]
+   
+
+module.exports = { plays, invoices  }
