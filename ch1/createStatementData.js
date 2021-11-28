ab6a89de94ec7151a0dd0b7c2e5d66bb5ac9a29d
rsgilbert <gssenyonjo@gmail.com>
2021-11-28 21:30:42 +0000

    tragedy get amount

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -47,6 +47,20 @@ class PerformanceCalculator {
     }
 }
 
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if(this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);            
+        }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+
+}
+
 
 export default function createStatementData(invoice, plays) {
     const result = {}
@@ -59,13 +73,25 @@ export default function createStatementData(invoice, plays) {
     
     // Enrich the performance record with data from the play
     // Pattern: Move function
+    // Pattern: Replace Constructor With Factory
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance))
         const result = Object.assign({}, aPerformance)
         result.play = calculator.play;
         result.amount = calculator.amount;
         result.volumeCredits = calculator.volumeCredits;
         return result;
+    }
+
+    function createPerformanceCalculator(aPerformance, aPlay) {
+        switch(aPlay.type) {
+            case 'tragedy':
+                return new TragedyCalculator(aPerformance, aPlay);
+            case 'comedy':
+                return new ComedyCalculator(aPerformance, aPlay);
+            default:
+                throw new Error(`unknown type: ${aPlay.type}`)
+        }
     }
 
     
