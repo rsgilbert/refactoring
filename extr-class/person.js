f964c087af94b48ff3fc9b59cf83db4aabbccc8f
rsgilbert <gssenyonjo@gmail.com>
2021-12-28 16:08:03 +0000

    renamed areacode"

--- extr-class/person.js
+++ extr-class/person.js
@@ -28,13 +28,28 @@ test.skip(function personTest() {
 // refactor 
 
 class Person2 {
+    constructor() {
+        this._telephoneNumber = new TelephoneNumber();
+    }
     get name() { return this._name; }
     set name(arg) { return this._name = arg; }
     get telephoneNumber() {
-        return `${this.officeAreaCode} ${this.officeNumber}`
+        return this._telephoneNumber.telephoneNumber;
     }
-    get officeAreaCode() { return this._officeAreaCode; }
-    set officeAreaCode(arg) { this._officeAreaCode = arg; }
+    get officeAreaCode() { return this._telephoneNumber.areaCode; }
+    set officeAreaCode(arg) { this._telephoneNumber.areaCode = arg; }
+
+    get officeNumber() { return this._telephoneNumber.officeNumber; }
+    set officeNumber(arg) { this._telephoneNumber.officeNumber = arg; }
+}
+
+class TelephoneNumber {
+    get telephoneNumber() {
+        return `${this.areaCode} ${this.officeNumber}`
+    }
+
+    get areaCode() { return this._officeAreaCode; }
+    set areaCode(arg) { this._officeAreaCode = arg; }
 
     get officeNumber() { return this._officeNumber; }
     set officeNumber(arg) { this._officeNumber = arg; }
--- repl-temp-with-query/repl-temp-with-query.md
+++ repl-temp-with-query/repl-temp-with-query.md
@@ -1,1 +1,4 @@
 # REPLACE TEMP WITH QUERY
+
+Replace a temporary variable with a function 
+that calculates the value of the variable.
